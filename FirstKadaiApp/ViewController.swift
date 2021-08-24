6d0804ba91f8d409c257a19a0b6b93955e7b1190
Takahiro Yano <after-schoolnana@i.softbank.jp>
2021-08-24 20:35:59 +0900

    Initial Commit

---
+++ FirstKadaiApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirstKadaiApp
+//
+//  Created by Yano Takahiro  on 2021/08/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
